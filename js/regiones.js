a2eb3b6c1083414f4f00465fba1a09d4ef29e285
solosiss <to.salasm@duocuc.cl>
2026-09-09 13:36:34 -0300

    agrega arreglo de regiones y comunas de Chile

---
+++ js/regiones.js
@@ -0,0 +1,73 @@
+/* ==========================================================
+   BOL Store - Regiones y comunas
+   El proyecto pide mostrar las regiones desde un arreglo de
+   JavaScript y que al cambiar la region se actualicen las
+   comunas que se muestran.
+   ========================================================== */
+
+var regiones = [
+  {
+    nombre: "Region de Arica y Parinacota",
+    comunas: ["Arica", "Camarones", "Putre", "General Lagos"]
+  },
+  {
+    nombre: "Region de Tarapaca",
+    comunas: ["Iquique", "Alto Hospicio", "Pozo Almonte", "Pica", "Huara"]
+  },
+  {
+    nombre: "Region de Antofagasta",
+    comunas: ["Antofagasta", "Calama", "Tocopilla", "Mejillones", "Taltal", "San Pedro de Atacama"]
+  },
+  {
+    nombre: "Region de Atacama",
+    comunas: ["Copiapo", "Vallenar", "Caldera", "Chanaral", "Huasco", "Tierra Amarilla"]
+  },
+  {
+    nombre: "Region de Coquimbo",
+    comunas: ["La Serena", "Coquimbo", "Ovalle", "Illapel", "Vicuna", "Los Vilos"]
+  },
+  {
+    nombre: "Region de Valparaiso",
+    comunas: ["Valparaiso", "Vina del Mar", "Quilpue", "Villa Alemana", "San Antonio", "Quillota", "Los Andes", "San Felipe"]
+  },
+  {
+    nombre: "Region Metropolitana de Santiago",
+    comunas: ["Santiago", "Providencia", "Las Condes", "Nunoa", "Maipu", "La Florida", "Puente Alto", "San Bernardo", "Recoleta", "Independencia", "Quilicura", "Renca"]
+  },
+  {
+    nombre: "Region del Libertador General Bernardo O'Higgins",
+    comunas: ["Rancagua", "San Fernando", "Santa Cruz", "Rengo", "Machali", "Pichilemu"]
+  },
+  {
+    nombre: "Region del Maule",
+    comunas: ["Talca", "Curico", "Linares", "Constitucion", "Cauquenes", "Molina"]
+  },
+  {
+    nombre: "Region de Nuble",
+    comunas: ["Chillan", "Chillan Viejo", "San Carlos", "Bulnes", "Quirihue", "Yungay"]
+  },
+  {
+    nombre: "Region del Biobio",
+    comunas: ["Concepcion", "Talcahuano", "Los Angeles", "Chiguayante", "San Pedro de la Paz", "Coronel", "Lota"]
+  },
+  {
+    nombre: "Region de La Araucania",
+    comunas: ["Temuco", "Padre Las Casas", "Villarrica", "Angol", "Pucon", "Victoria"]
+  },
+  {
+    nombre: "Region de Los Rios",
+    comunas: ["Valdivia", "La Union", "Panguipulli", "Rio Bueno", "Lanco", "Futrono"]
+  },
+  {
+    nombre: "Region de Los Lagos",
+    comunas: ["Puerto Montt", "Osorno", "Castro", "Ancud", "Puerto Varas", "Quellon"]
+  },
+  {
+    nombre: "Region de Aysen del General Carlos Ibanez del Campo",
+    comunas: ["Coyhaique", "Puerto Aysen", "Chile Chico", "Cochrane", "Puerto Cisnes"]
+  },
+  {
+    nombre: "Region de Magallanes y de la Antartica Chilena",
+    comunas: ["Punta Arenas", "Puerto Natales", "Porvenir", "Cabo de Hornos", "Primavera"]
+  }
+];
